ac407fdc365f8ef7bebd866985662ddf3c268d78
Viet Pham <vietpham2017@gmail.com>
2025-10-01 17:32:19 -0500

    progress

--- src/api/v1/services/auth.services.ts
+++ src/api/v1/services/auth.services.ts
@@ -32,7 +32,7 @@ const AUTH_CONSTANTS = {
 const DEFAULT_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
