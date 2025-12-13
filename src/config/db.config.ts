fd99138c3766ca2c052bbbaafc3d7e65a4707890
Viet Pham <vietpham2017@gmail.com>
2025-12-12 22:37:08 -0600

    progress

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -8,7 +8,7 @@ const pool: Pool = mysql.createPool({
   database: process.env.DATABASE_NAME as string,
   user: process.env.USER_NAME as string,
   password: process.env.PASSWORD as string,
-  timezone: "Z", // Store and retrieve dates in UTC
+  timezone: "-06:00", // or 'America/Chicago' for Central Time
 });
 
 const connectDB = async () => {
